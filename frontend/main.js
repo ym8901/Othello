69f49a2036e1a5d6641ac579ebabd63fa7312ba9
YasudaMasato <ym20038901018@gmail.com>
2023-06-07 13:02:09 +0900

    7commit

--- frontend/main.js
+++ frontend/main.js
@@ -5,6 +5,35 @@ let data = [];
 const board = document.getElementById("board");
 const h2 = document.querySelector("h2");
 const counter = document.getElementById("counter");
+document
+  .querySelectorAll('[name="b-selector"],[name="w-selector"]')
+  .forEach((value) => {
+    value.addEventListener(`change`, setDisabled);
+  });
+document.querySelectorAll(".select").forEach((value) => {
+  value.addEventListener("click", start);
+});
+
+// 特定のラジオボタンが選択された時、一部のラジオボタンとテキストボックスを無効化する
+function setDisabled(e) {
+  var isAnyChecked = false;
+  var radioGroups = document.querySelectorAll(
+    '[id="b-h-option"],[id="w-h-option"]'
+  );
+  var targetElement = document.getElementById("menu");
+
+  radioGroups.forEach(function (radio) {
+    if (radio.checked) {
+      isAnyChecked = true;
+    }
+  });
+
+  if (isAnyChecked) {
+    targetElement.classList.add("disabled");
+  } else {
+    targetElement.classList.remove("disabled");
+  }
+}
 
 let board_size = 6; //盤面のサイズ
 
